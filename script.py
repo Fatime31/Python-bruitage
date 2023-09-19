e1e1a4d3cf53f1d3f680fae097f8f21adee5e140
Léo <leo.babaux@etu.iut-tlse3.fr>
2023-09-19 09:20:57 +0200

    filtrage

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ Created on Thu Sep  7 12:15:25 2023
 @author: ndiayefatime
 """
 from skimage import  io
-from skimage.color import rgb2gray
+import statistics as stat
 import numpy as np
 import random
 
@@ -34,14 +34,40 @@ def bruitMultiplicatif(niveauBruit,image):
             image[x,y] = min(max(image[x,y]*(1+(bruit[x,y]*niveauBruit)),0),255)
     return image
 
+def filtrageConvolution(rayon, image):
+    for x in range(image.shape[0]):
+        for y in range(image.shape[1]):
+            voisinage = []
+            for x2 in range(x-rayon,x+rayon+1,1):
+                for y2 in range(y-rayon,y+rayon+1,1):
+                    if (x2 >= 0 and y2 >= 0 and x2 < image.shape[0] and y2 < image.shape[1]):
+                        voisinage.append(image[x2,y2])
+            image[x,y] = stat.mean(voisinage)
+    return image
 
+def filtrageMedian(rayon, image):
+    for x in range(image.shape[0]):
+        for y in range(image.shape[1]):
+            voisinage = []
+            for x2 in range(x-rayon,x+rayon+1,1):
+                for y2 in range(y-rayon,y+rayon+1,1):
+                    if (x2 >= 0 and y2 >= 0 and x2 < image.shape[0] and y2 < image.shape[1]):
+                        voisinage.append(image[x2,y2])
+            image[x,y] = stat.median(voisinage)
+    return image
+
+#------------------------------------------------------------------------------
 
 img="Reference_pour_SNR/dcode-image.png"
+img2="Reference_pour_SNR/image1_reference.png"
 
 image = io.imread(img)
 
-#io.imshow(saltPepper(0.1, image))
+#io.imshow(filtrageMedian(1,saltPepper(0.1, image)))
 
 #io.imshow(bruitAdditif(20, image))
+#io.imshow(filtrageConvolution(1,bruitAdditif(20, image)))
 
-io.imshow(bruitMultiplicatif(0.3, image))
+#io.imshow(bruitMultiplicatif(0.3, image))
+
+
